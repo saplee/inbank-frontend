8fe5803f18fe3adc06f673b8d2ec3fff855b4e2a
saplee <saplee@ttu.ee>
2023-03-27 14:58:50 +0300

    New about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is my frontend project.</h1>
+    <h2>Author: Sander Pleesi</h2>
+    <ul>
+      <a href="https://github.com/saplee/inbank-frontend">Frontend repo</a>
+      <br>
+      <a href="https://github.com/saplee/inbank-backend">Backend repo</a>
+    </ul>
   </div>
 </template>
 
 <style>
+.about {
+  text-align: center;
+  margin: 10em;
+}
 </style>
